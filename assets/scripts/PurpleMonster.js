e346f2a28f17ad60c0a638a469bbdf798986c095
e <rulanfenghua@163.com>
2018-11-12 18:15:32 +0800

    eslint

--- assets/scripts/PurpleMonster.js
+++ assets/scripts/PurpleMonster.js
@@ -27,15 +27,43 @@ cc.Class({
     //         this._bar = value;
     //     }
     // },
+
+    // 主角跳跃高度
+    jumpHeight: 0,
+    // 主角跳跃持续时间
+    jumpDuration: 0,
+    // 最大移动速度
+    maxMoveSpeed: 0,
+    // 加速度
+    accel: 0
+  },
+
+  setJumpAction() {
+    // 跳跃上升
+    var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+    // 下落
+    var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+    // 不断重复
+    return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+  },
+
+  onKeyDown(event) {
+    switch(event.keyCode) {
+      case 
+    }
   },
 
   // LIFE-CYCLE CALLBACKS:
 
-  // onLoad () {},
+  onLoad() {
+    // 初始化跳跃动作
+    this.jumpAction = this.setJumpAction();
+    this.node.runAction(this.jumpAction);
+  },
 
-  start () {
+  start() {
 
   }
 
   // update (dt) {},
-})
+});
