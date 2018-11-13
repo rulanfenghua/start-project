07ac1a1eec455116940d506b9fb3d55a5482d6a6
e <rulanfenghua@163.com>
2018-11-13 18:40:48 +0800

    player

--- assets/scripts/PurpleMonster.js
+++ assets/scripts/PurpleMonster.js
@@ -40,16 +40,32 @@ cc.Class({
 
   setJumpAction() {
     // 跳跃上升
-    var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+    var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut())
     // 下落
-    var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+    var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn())
     // 不断重复
-    return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    return cc.repeatForever(cc.sequence(jumpUp, jumpDown))
   },
 
   onKeyDown(event) {
-    switch(event.keyCode) {
-      case 
+    switch (event.keyCode) {
+      case cc.macro.KEY.a:
+        this.accLeft = true
+        break
+      case cc.macro.KEY.d:
+        this.accRight = true
+        break
+    }
+  },
+
+  onKeyUp(event) {
+    switch (event.keyCode) {
+      case cc.macro.KEY.a:
+        this.accLeft = false
+        break
+      case cc.macro.KEY.d:
+        this.accRight = false
+        break
     }
   },
 
@@ -57,13 +73,42 @@ cc.Class({
 
   onLoad() {
     // 初始化跳跃动作
-    this.jumpAction = this.setJumpAction();
-    this.node.runAction(this.jumpAction);
+    this.jumpAction = this.setJumpAction()
+    this.node.runAction(this.jumpAction)
+
+    // 加速度方向开关
+    this.accLeft = false
+    this.accRight = false
+    // 主角当前水平方向速度
+    this.xSpeed = 0
+
+    // 初始化键盘输入监听
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this)
+    cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this)
+  },
+
+  onDestroy() {
+    // 取消键盘输入监听
+    cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.offKeyDown, this)
+    cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this)
   },
 
   start() {
 
-  }
+  },
 
-  // update (dt) {},
-});
+  update(dt) {
+    if (this.accLeft) {
+      this.xSpeed -= this.accel * dt
+    } else if (this.accRight) {
+      this.xSpeed += this.accel * dt
+    }
+
+    if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+      // if speed reach limit, use max speed with current direction 给予一个正确方向
+      this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed)
+    }
+
+    this.node.x += this.xSpeed * dt
+  }
+})
